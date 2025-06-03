e3bb5de1795953562c693be781841bedbf50eec3
Harshal Khairnar <harshalkhairnar7244@gmail.com>
2025-06-03 15:35:13 +0530

    github added

--- src/app/components/SocialLinks/SocialLinks.jsx
+++ src/app/components/SocialLinks/SocialLinks.jsx
@@ -11,11 +11,20 @@ import SLink from "./SLink";
 export default function SocialLinks() {
   return (
     <div className=" flex lg:flex-col gap-10 lg:gap-6 justify-center items-center pt-3 z-30">
-      <SLink Icon={FaLinkedinIn} link="https://www.linkedin.com/in/harshal-khairnar0" />
-      {/* <SLink Icon={FaGithub} link="#" /> */}
+      <SLink
+        Icon={FaLinkedinIn}
+        link="https://www.linkedin.com/in/harshal-khairnar0"
+      />
+      <SLink Icon={FaGithub} link="https://github.com/Harshal-Khairnar01" />
       <SLink Icon={FaTwitter} link="https://twitter.com/HarshalKha25502" />
-      <SLink Icon={FaInstagram} link="https://www.instagram.com/harshal.khairnar_" />
-      <SLink Icon={FaFacebookF} link="https://www.facebook.com/harshal.khairnar.984" />
+      <SLink
+        Icon={FaInstagram}
+        link="https://www.instagram.com/harshal.khairnar_"
+      />
+      <SLink
+        Icon={FaFacebookF}
+        link="https://www.facebook.com/harshal.khairnar.984"
+      />
     </div>
   );
 }
